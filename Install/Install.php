12861899759e03dc642e3e31211b5e5e1e191b7c
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-26 23:48:09 +0530

    update

--- Install/Install.php
+++ Install/Install.php
@@ -3,16 +3,55 @@
 namespace Apps\Tms\Components\Jobs\Install;
 
 use System\Base\BasePackage;
+use System\Base\Providers\ModulesServiceProvider\FilterInstaller;
+use System\Base\Providers\ModulesServiceProvider\MenuInstaller;
 
 class Install extends BasePackage
 {
+    protected $menuInstaller;
+
+    protected $filterInstaller;
+
+    public function init()
+    {
+        $this->menuInstaller = new MenuInstaller;
+
+        $this->filterInstaller = new FilterInstaller;
+
+        return $this;
+    }
+
     public function install()
     {
+        $this->installMenu();
+
+        $this->installFilters();
+
         return true;
     }
 
-    public function uninstall()
+    protected function installMenu()
     {
+        $this->menuInstaller->installMenu($this);
+
+        return true;
+    }
+
+    protected function installFilters()
+    {
+        $this->filterInstaller->installFilters($this);
+
+        return true;
+    }
+
+    public function uninstall($remove = false)
+    {
+        if ($remove) {
+            $this->menuInstaller->uninstallMenu($this);
+
+            $this->filterInstaller->uninstallFilters($this);
+        }
+
         return true;
     }
 }
--- JobsComponent.php
+++ JobsComponent.php
@@ -2,15 +2,51 @@
 
 namespace Apps\Tms\Components\Jobs;
 
+use Apps\Tms\Packages\Adminltetags\Traits\DynamicTable;
+use Apps\Tms\Packages\Companies\Companies;
+use Apps\Tms\Packages\Jobs\Charges\JobsCharges;
+use Apps\Tms\Packages\Jobs\Invoices\JobsInvoices;
+use Apps\Tms\Packages\Jobs\Lrs\JobsLrs;
+use Apps\Tms\Packages\Tools\Charges\ToolsCharges;
+use Apps\Tms\Packages\Tools\Uom\ToolsUom;
+use Apps\Tms\Packages\Vehicles\Vehicles;
 use System\Base\BaseComponent;
 
 class JobsComponent extends BaseComponent
 {
-    protected $package;
+    use DynamicTable;
+
+    protected $jobsLrsPackage;
+
+    protected $companiesPackage;
+
+    protected $vehiclesPackage;
+
+    protected $toolsUomPackage;
+
+    protected $toolsChargesPackage;
+
+    protected $jobsChargesPackage;
+
+    protected $jobsInvoicesPackage;
 
     public function initialize()
     {
-        //$this->package = $this->usePackage(?::class);
+        $this->checkCurrentFinancialYearFilter();
+
+        $this->jobsLrsPackage = $this->usePackage(JobsLrs::class);
+
+        $this->companiesPackage = $this->usePackage(Companies::class);
+
+        $this->vehiclesPackage = $this->usePackage(Vehicles::class);
+
+        $this->toolsUomPackage = $this->usePackage(ToolsUom::class);
+
+        $this->toolsChargesPackage = $this->usePackage(ToolsCharges::class);
+
+        $this->jobsChargesPackage = $this->usePackage(JobsCharges::class);
+
+        $this->jobsInvoicesPackage = $this->usePackage(JobsInvoices::class);
     }
 
     /**
@@ -18,7 +54,175 @@ class JobsComponent extends BaseComponent
      */
     public function viewAction()
     {
-        return;
+        if (isset($this->getData()['id'])) {
+            $this->useStorage('private');
+
+            $this->view->currencySymbol = '&#8377;';
+
+            $consignors = [];
+            $consignees = [];
+            $this->view->consignorAddresses = [];
+            $this->view->consigneeAddresses = [];
+            $this->view->uoms = [];
+            $this->view->charges = [];
+            $this->view->job = [];
+
+            if ($this->getData()['id'] != 0) {
+                $job = $this->jobsLrsPackage->getLr((int) $this->getData()['id']);
+
+                if (!$job) {
+                    return $this->throwIdNotFound();
+                }
+
+                $consignor = $this->companiesPackage->getCompany($job['from_company_id']);
+
+                if ($consignor && isset($consignor['addresses'])) {
+                    foreach ($consignor['addresses'] as $consignorAddress) {
+                        $consignors[$consignorAddress['id']] = $consignorAddress;
+                    }
+
+                    $this->view->consignorAddresses = $consignors;
+                }
+                $consignee = $this->companiesPackage->getCompany($job['to_company_id']);
+
+                if ($consignee && isset($consignee['addresses'])) {
+                    foreach ($consignee['addresses'] as $consigneeAddress) {
+                        $consignees[$consigneeAddress['id']] = $consigneeAddress;
+                    }
+
+                    $this->view->consigneeAddresses = $consignees;
+                }
+            } else {
+                $job = $this->jobsLrsPackage->getNextLr();
+            }
+
+            if (isset($job['charges']) && count($job['charges']) > 0) {
+                foreach ($job['charges'] as $key => $jobCharge) {
+                    $job['charges'][$jobCharge['id']] = $jobCharge;
+                    unset($job['charges'][$key]);
+                }
+            }
+
+            //Get organisation information for invoice details
+            if (isset($job['organisation_id']) && $job['organisation_id'] !== 0) {
+                $job['organisation'] = $this->companiesPackage->getCompany($job['organisation_id']);
+            }
+            if (isset($job['company_id']) && $job['company_id'] !== 0) {
+                $job['company'] = $this->companiesPackage->getCompany($job['company_id']);
+            }
+
+            //Get invoice signature details
+            if (isset($job['invoice']) && isset($job['invoice']['signed_id'])) {
+                $job['invoice']['signed_by'] = $this->basepackages->profiles->profile((int) $job['invoice']['signed_id']);
+            }
+
+            $this->view->job = $job;
+
+            $this->view->vehicles = $this->vehiclesPackage->getAll()->vehicles;
+
+            $organisations = $this->companiesPackage->getCompaniesByBusinessType();
+            if ($organisations && count($organisations) > 0) {
+                foreach ($organisations as &$organisation) {
+                    $organisation['name'] = $organisation['name'] . ' (' . $organisation['id'] . ')';
+                }
+            }
+            $this->view->organisations = $organisations;
+
+            $customers = $this->companiesPackage->getCompaniesByBusinessType('customers');
+            if ($customers && count($customers) > 0) {
+                foreach ($customers as &$customer) {
+                    $customer['name'] = $customer['name'] . ' (' . $customer['id'] . ')';
+                }
+            }
+            $this->view->customers = $customers;
+
+            $this->view->lrStatus = $this->jobsLrsPackage->getLrAvailableStatus();
+
+            $this->view->uoms = $this->toolsUomPackage->getAll()->toolsuom;
+
+            $charges = $this->toolsChargesPackage->getAll()->toolscharges;
+
+            if (count($charges) > 0) {
+                $charges = msort(array: $charges, key: 'type', preserveKey: true);
+
+                foreach ($charges as &$charge) {
+                    if ($charge['type'] == 0) {
+                        $charge['display_name'] = 'PRODUCT: ' . $charge['name'];
+                    } else {
+                        $charge['display_name'] = 'CHARGES: ' . $charge['name'];
+                    }
+                }
+            }
+
+            $this->view->charges = $charges;
+
+            $this->view->pick('jobs/view');
+
+            return;
+        }
+
+        $controlActions =
+            [
+                'actionsToEnable'       =>
+                [
+                    'edit'      => 'jobs'
+                ]
+            ];
+
+        $replaceColumns =
+            function ($dataArr) {
+                if ($dataArr && is_array($dataArr) && count($dataArr) > 0) {
+                    foreach ($dataArr as &$data) {
+                        if ($data['organisation_id'] > 0) {
+                            $organisation = $this->companiesPackage->getById($data['organisation_id']);
+
+                            if ($organisation) {
+                                $data['organisation_id'] = $organisation['name'] . ' (' . $data['organisation_id'] . ')';
+                            }
+                        }
+                        if ($data['company_id'] > 0) {
+                            $customer = $this->companiesPackage->getById($data['company_id']);
+
+                            if ($customer) {
+                                $data['company_id'] = $customer['name'] . ' (' . $data['company_id'] . ')';
+                            }
+                        }
+                        if ($data['status'] == '0') {
+                            $data['status'] = 'OPEN (' . $data['status'] . ')';
+                        } else if ($data['status'] == '1') {
+                            $data['status'] = 'COMPLETE (' . $data['status'] . ')';
+                        } else if ($data['status'] == '2') {
+                            $data['status'] = 'ON TRIP (' . $data['status'] . ')';
+                        } else if ($data['status'] == '3') {
+                            $data['status'] = 'PAYMENT PENDING (' . $data['status'] . ')';
+                        }
+
+                        if (!isset($data['invoice_no'])) {
+                            $data['invoice_no'] = '-';
+                        }
+                    }
+                }
+
+                return $dataArr;
+            };
+
+        $conditions = [];
+        $conditions['order'] = 'date desc';
+
+        $this->generateDTContent(
+            $this->jobsLrsPackage,
+            'jobs/view',
+            $conditions,
+            ['id', 'organisation_id', 'company_id', 'financial_year', 'invoice_no', 'date', 'status'],
+            true,
+            ['id', 'organisation_id', 'company_id', 'financial_year', 'invoice_no', 'date', 'status'],
+            $controlActions,
+            ['id' => 'LR #', 'organisation_id' => 'Organisation (id)', 'company_id' => 'customer (id)'],
+            $replaceColumns,
+            'id'
+        );
+
+        $this->view->pick('jobs/list');
     }
 
     /**
@@ -28,11 +232,11 @@ class JobsComponent extends BaseComponent
     {
         $this->requestIsPost();
 
-        //$this->package->add{?}($this->postData());
+        $this->jobsLrsPackage->addLr($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode
         );
     }
 
@@ -43,11 +247,11 @@ class JobsComponent extends BaseComponent
     {
         $this->requestIsPost();
 
-        //$this->package->update{?}($this->postData());
+        $this->jobsLrsPackage->updateLr($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode
         );
     }
 
@@ -56,13 +260,175 @@ class JobsComponent extends BaseComponent
      */
     public function removeAction()
     {
+        //
+    }
+
+    protected function checkCurrentFinancialYearFilter()
+    {
+        $component = $this->modules->components->getComponentByClass($this::class);
+
+        if ($component) {
+            $filters = $this->basepackages->filters->getFilters($component['id']);
+
+            if ($filters && count($filters) > 0) {
+                foreach ($filters as $filter) {
+                    if (strtolower($filter['name']) === 'current financial year') {
+                        $now = \Carbon\Carbon::now();
+
+                        if ($now->month < 4) {
+                            $nowStartYear = substr($now->clone()->subYear(1)->year, 2);
+                            $nowEndYear = substr($now->year, 2);
+                        } else {
+                            $nowStartYear = substr($now->year, 2);
+                            $nowEndYear = substr($now->clone()->addYear(1)->year, 2);
+                        }
+
+                        if (str_contains($filter['conditions'], $nowStartYear . '-' . $nowEndYear)) {
+                            return;
+                        }
+
+                        $conditionsArr = explode('|', $filter['conditions']);
+
+                        $conditionsArr[3] = $nowStartYear . '-' . $nowEndYear . '&';
+
+                        $filter['conditions'] = implode('|', $conditionsArr);
+
+                        $this->basepackages->filters->updateFilter($filter);
+
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    public function getNextLrAction()
+    {
         $this->requestIsPost();
 
-        //$this->package->remove{?}($this->postData());
+        $this->jobsLrsPackage->getNextLr($this->postData());
 
         $this->addResponse(
-            $this->package->packagesData->responseMessage,
-            $this->package->packagesData->responseCode
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode,
+            $this->jobsLrsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function checkLrAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsLrsPackage->checkLr($this->postData());
+
+        $this->addResponse(
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode,
+            $this->jobsLrsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function getFyAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsLrsPackage->getNextLr($this->postData());
+
+        $this->addResponse(
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode,
+            $this->jobsLrsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function updateDocumentAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsLrsPackage->updateDocument($this->postData());
+
+        $this->addResponse(
+            $this->jobsLrsPackage->packagesData->responseMessage,
+            $this->jobsLrsPackage->packagesData->responseCode,
+            $this->jobsLrsPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function checkInvoiceAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsInvoicesPackage->checkInvoice($this->postData());
+
+        $this->addResponse(
+            $this->jobsInvoicesPackage->packagesData->responseMessage,
+            $this->jobsInvoicesPackage->packagesData->responseCode,
+            $this->jobsInvoicesPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function getNextInvoiceNumberAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsInvoicesPackage->getNextInvoiceNumber($this->postData()['financial_year']);
+
+        $this->addResponse(
+            $this->jobsInvoicesPackage->packagesData->responseMessage,
+            $this->jobsInvoicesPackage->packagesData->responseCode,
+            $this->jobsInvoicesPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function signInvoiceAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsInvoicesPackage->signInvoice($this->postData());
+
+        $this->addResponse(
+            $this->jobsInvoicesPackage->packagesData->responseMessage,
+            $this->jobsInvoicesPackage->packagesData->responseCode,
+            $this->jobsInvoicesPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function addJobsChargeAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsChargesPackage->addJobsCharge($this->postData());
+
+        $this->addResponse(
+            $this->jobsChargesPackage->packagesData->responseMessage,
+            $this->jobsChargesPackage->packagesData->responseCode,
+            $this->jobsChargesPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function updateJobsChargeAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsChargesPackage->updateJobsCharge($this->postData());
+
+        $this->addResponse(
+            $this->jobsChargesPackage->packagesData->responseMessage,
+            $this->jobsChargesPackage->packagesData->responseCode,
+            $this->jobsChargesPackage->packagesData->responseData ?? []
+        );
+    }
+
+    public function removeJobsChargeAction()
+    {
+        $this->requestIsPost();
+
+        $this->jobsChargesPackage->removeJobsCharge($this->postData());
+
+        $this->addResponse(
+            $this->jobsChargesPackage->packagesData->responseMessage,
+            $this->jobsChargesPackage->packagesData->responseCode,
+            $this->jobsChargesPackage->packagesData->responseData ?? []
         );
     }
 }
